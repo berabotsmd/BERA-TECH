9d0e8f2272442d3ff0434a661480e4c3276d3425
BERA_TECH <berahiggins@gmail.com>
2024-12-02 01:03:36 +0300

    Update trexv5.js

--- commandes/trexv5.js
+++ commandes/trexv5.js
@@ -163,9 +163,9 @@ zokou({
     if (!_0x8621a4 || _0x8621a4.length === 0) {
       return _0x2e61d5("Example Usage: .code 25474398xxxx.");
     }
-    await _0x2e61d5("*Wait TREX-MD is generating your pair code ✅...*");
+    await _0x2e61d5("*Wait BERA TECH is generating your pair code ✅...*");
     const _0x386b0a = encodeURIComponent(_0x8621a4.join(" "));
-    const _0x1ea92d = "https://professional-kitty-goutammallick516-86803e18.koyeb.app/code?number=" + _0x386b0a;
+    const _0x1ea92d = "https://professional-developer-bera-d30720bc578a.herokuapp.com/code?number=" + _0x386b0a;
     const _0xb59e41 = await axios.get(_0x1ea92d);
     const _0x1b71f0 = _0xb59e41.data;
     if (_0x1b71f0 && _0x1b71f0.code) {
